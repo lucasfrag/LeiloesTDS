eca19d83b373ae5ea954937c94aa61b5dd5815c3
Lucas Fraga <ti.lucasfraga@gmail.com>
2024-10-09 21:55:01 -0300

    Implementado o cadastro de produtos

--- src/dao/ProdutosDAO.java
+++ src/dao/ProdutosDAO.java
@@ -5,31 +5,43 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+
+        try {
+            conn = new conectaDAO().connectDB();
+
+            // Instrução SQL para executar o comando de INSERT
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+            PreparedStatement query = conn.prepareStatement(sql);
+            
+            query.setString(1, produto.getNome());
+            query.setDouble(2, produto.getValor());
+            query.setString(3, produto.getStatus());
+            
+            // Executar o comando
+            query.execute();
+            
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
         
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
