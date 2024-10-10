a749a171b9d9979657ee286c7811fb547c0b9b53
Lucas Fraga <ti.lucasfraga@gmail.com>
2024-10-09 21:10:00 -0300

    Definida a tela inicial

---
+++ src/Principal.java
@@ -0,0 +1,11 @@
+
+import view.cadastroVIEW;
+
+
+public class Principal {
+    public static void main(String args[]) {
+        cadastroVIEW tela = new cadastroVIEW();
+        tela.setVisible(true);
+    }
+    
+}
