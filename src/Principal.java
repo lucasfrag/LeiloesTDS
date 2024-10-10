cd630d4edfd6b4a27c2f515c4a3f498327b089fb
Lucas Fraga <ti.lucasfraga@gmail.com>
2024-10-09 21:55:48 -0300

    Adicionado exemplo de cadastro para teste

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
 
+import dao.ProdutosDAO;
+import dto.ProdutosDTO;
 import view.cadastroVIEW;
 
 
@@ -6,6 +8,16 @@ public class Principal {
     public static void main(String args[]) {
         cadastroVIEW tela = new cadastroVIEW();
         tela.setVisible(true);
+        
+        /*
+        ProdutosDTO p = new ProdutosDTO();
+        p.setNome("Teste");
+        p.setValor(350.0);
+        p.setStatus("A Venda");
+        
+        ProdutosDAO dao = new ProdutosDAO();
+        dao.cadastrarProduto(p);
+        */
     }
     
 }
