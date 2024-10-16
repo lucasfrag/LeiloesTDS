12cc3e9c602defcf4ec76c2c18d75b700d93ed8b
Lucas Fraga <ti.lucasfraga@gmail.com>
2024-10-16 20:55:38 -0300

    Removido comentário na tela inicial

--- src/Principal.java
+++ src/Principal.java
@@ -8,16 +8,7 @@ public class Principal {
     public static void main(String args[]) {
         cadastroVIEW tela = new cadastroVIEW();
         tela.setVisible(true);
-        
-        /*
-        ProdutosDTO p = new ProdutosDTO();
-        p.setNome("Teste");
-        p.setValor(350.0);
-        p.setStatus("A Venda");
-        
-        ProdutosDAO dao = new ProdutosDAO();
-        dao.cadastrarProduto(p);
-        */
+
     }
     
 }
